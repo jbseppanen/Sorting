7c9eb62b5045aaddb8520eedeed1edf527b6e80a
Jacob Seppanen <jbseppanen@gmail.com>
2019-04-30 16:04:46 -0700

    Attempt at implementing methods in searching.py  Binary_search_recursive not complete.

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,30 +1,48 @@
 # STRETCH: implement Linear Search				
 def linear_search(arr, target):
-  
-  # TO-DO: add missing code
+    for a in arr:
+        if a == target:
+            return a
 
-   return -1   # not found
+    return -1  # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
 def binary_search(arr, target):
+    if len(arr) == 0:
+        return -1  # array empty
 
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
+    # TO-DO: add missing code
+    while len(arr) > 0:
+        mid = int(len(arr) / 2)
+        mid_test = arr[mid]
+        if mid_test == target:
+            return target
+        elif mid_test > target:
+            arr = arr[:mid]
+        else:
+            arr = arr[mid + 1:]
 
-  # TO-DO: add missing code
-
-  return -1 # not found
+    return -1  # not found
 
 
 # STRETCH: write a recursive implementation of Binary Search 
-def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)//2
+def binary_search_recursive(arr, target):
+    # middle = (low + high) // 2
+    if len(arr) == 0:
+        return -1  # array empty
+    # TO-DO: add missing if/else statements, recursive calls
+    while len(arr) > 1:
+        middle = int(len(arr) / 2)
+        if arr[middle] == target:
+            arr = arr[middle:middle + 1]
+        elif arr[middle] > target:
+            arr = binary_search_recursive(arr[:middle], target)
+        else:
+            arr = binary_search_recursive(arr[middle + 1:], target)
+    return arr
 
-  if len(arr) == 0:
-    return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+
+ar = [1, 2, 3, 4, 5, 65, 7, 8, 9, 10, 11, 12, 13]
+# print(binary_search(ar, 6))
+print(binary_search_recursive(ar, 6))
